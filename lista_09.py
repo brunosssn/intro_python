1f414819f5401d161c1f45a0edc777a2153033fb
Bruno Sena <eubrunosena2@gmail.com>
2025-02-20 17:27:11 -0300

    first commit

--- lista_09.py
+++ lista_09.py
@@ -1,1 +1,15 @@
-print("Teste")
+#Questão 1
+def soma(a, b):
+    print(a + b)
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+soma(n1, n2)
+
+#Questão 2
+def épar(x):
+ return(x%2==0)
+def par_ou_ímpar(x):
+ if épar(x):
+     return "par"
+ else:
+     return "ímpar"
